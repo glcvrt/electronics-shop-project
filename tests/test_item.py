f7bb43b2e6b628206d0fdd2dce33030b18b57807
glcvrt <i1472138@yandex.ru>
2023-09-13 21:06:28 +0700

    add item class and test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 2)
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 20000
+
+def test_apply_discount(item1):
+    item1.pay_rate = 0.7
+    item1.apply_discount()
+    assert item1.price == 7000
