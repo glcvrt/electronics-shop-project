d01c0a6b43896c800b5e30e3cd04a01bb52ed610
glcvrt <i1472138@yandex.ru>
2023-09-15 19:46:32 +0700

    new func

--- tests/test_item.py
+++ tests/test_item.py
@@ -12,3 +12,12 @@ def test_apply_discount(item1):
     item1.pay_rate = 0.7
     item1.apply_discount()
     assert item1.price == 7000
+
+def test_name(item1):
+    assert item1.name() == "Смартфон"
+
+def test_string_to_number(item1):
+    assert item1.string_to_number("15") == 15
+
+def test_instantiate_from_csv(item1):
+    assert item1.instantiate_from_csv('src/items.csv') == 10
