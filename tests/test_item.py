9ccdfea9adc225a82a20b25b76781719ea9319b4
glcvrt <i1472138@yandex.ru>
2023-09-25 20:56:29 +0700

    homework 4

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,29 +1,37 @@
 import pytest
 from src.item import Item
 
+
 @pytest.fixture
 def item1():
     return Item("Смартфон", 10000, 2)
 
+
 def test_calculate_total_price(item1):
     assert item1.calculate_total_price() == 20000
+
 
 def test_apply_discount(item1):
     item1.pay_rate = 0.7
     item1.apply_discount()
     assert item1.price == 7000
 
+
 def test_name(item1):
-    assert item1.name() == "Смартфон"
+    assert item1.name == "Смартфон"
+
 
 def test_string_to_number(item1):
     assert item1.string_to_number("15") == 15
 
+
 def test_instantiate_from_csv(item1):
     assert item1.instantiate_from_csv('src/items.csv') == 10
 
+
 def test_repr(item1):
     assert repr(item1) == "Item('Смартфон', 10000, 2)"
+
 
 def test_str(item1):
     assert str(item1) == 'Смартфон'
---
+++ tests/test_phone.py
@@ -0,0 +1,14 @@
+from src.item import Item
+from src.phone import Phone
+
+
+def test_add():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+
+
+def test_repr():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
