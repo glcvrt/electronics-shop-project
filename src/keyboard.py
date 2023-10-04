479e9f931e5b26e7f6f93649c50b9d5d49bd5303
nikita <i1472138@yandex.ru>
2023-10-04 18:12:57 +0300

    fix homework5

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,18 +4,33 @@ from src.item import Item
 class MixinLog:
 
     def __init__(self):
-        self.language = "EN"
+        self._language = 'EN'
+        super().__init__()
 
     def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
-        elif self.language == "RU":
-            self.language = "EN"
+        if self._language == "EN":
+            self._language = "RU"
+        else:
+            self._language = "EN"
+        return self
 
 
 class Keyboard(Item, MixinLog):
 
     def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
-        self.language = "EN"
+        self._language = "EN"
+
+    def __str__(self):
+        return self.name
+
+    def change_lang(self):
+        if self._language == "EN":
+            self._language = "RU"
+        else:
+            self._language = "EN"
+        return self
+    @property
+    def language(self):
+        return self._language
 
